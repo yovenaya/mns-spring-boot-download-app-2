7db4040d861044595c9ee2e81fe390e804384733
yoven.ayassamy <yoven.ayassamy@mns.mu>
2023-10-21 09:45:46 +0400

    First commit

---
+++ src/test/java/mu/mns/demo/download/app2/App2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.mns.demo.download.app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

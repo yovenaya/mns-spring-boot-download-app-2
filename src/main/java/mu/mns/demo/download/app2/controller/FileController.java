f3dba8232633af82375ab9695c19fa5a78d948eb
yoven.ayassamy <yoven.ayassamy@mns.mu>
2023-10-21 17:09:03 +0400

    Add endpoint to save a file

--- src/main/java/mu/mns/demo/download/app2/controller/FileController.java
+++ src/main/java/mu/mns/demo/download/app2/controller/FileController.java
@@ -1,10 +1,13 @@
 package mu.mns.demo.download.app2.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
 
 @Slf4j
 @RestController
@@ -12,8 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class FileController {
 
+    @Value("${mns.demo.document.path}")
+    private String path;
+
     @GetMapping
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/create-test-file")
+    public void createTestFile(String filename, String content) {
+        String path = this.path + File.separator + filename;
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            bw.write(content);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
